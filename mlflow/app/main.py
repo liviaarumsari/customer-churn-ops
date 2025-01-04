83e200912a4b85a84a46754f25af570aa5bf4586
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2025-01-04 12:57:02 +0700

    chore: update mlflow

--- mlflow/app/main.py
+++ mlflow/app/main.py
@@ -1,14 +1,15 @@
 import mlflow
 
+MLFLOW_TRACKING_URL = 'http://mlflow-service.default.svc.cluster.local:8002'
+
 def main():
     # Set MLflow tracking URI
-    mlflow.set_tracking_uri("http://mlflow-service.default.svc.cluster.local:5000")
+    mlflow.set_tracking_uri(MLFLOW_TRACKING_URL)
 
     # Start a new MLflow run
     with mlflow.start_run():
-        print("Simulating a pipeline...")
-        mlflow.log_param("param1", "value1")  # Log a mock parameter
-        mlflow.log_metric("metric1", 0.0)    # Log a mock metric
+        mlflow.log_param("learning_rate", 0.01)
+        mlflow.log_metric("accuracy", 0.95)
 
     print("Mock pipeline completed!")
 
